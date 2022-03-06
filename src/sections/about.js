50553921f29f39ba133d4d456c6973f5e33247e8
Siddanth Nayak <nayak@Siddanths-MacBook-Air.local>
2022-03-06 17:35:34 +0530

    Margin Changes

--- src/sections/about.js
+++ src/sections/about.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import styled from 'styled-components'
 
 const StyledAboutSection = styled.section`
-    margin: 10em 0em;
+    margin: 10em auto;
 `;
 
 const StyledText = styled.div`
